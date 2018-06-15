af71e2528abdbf1b687bb007ac19c9ad3ea59a26
iluiscastillo <luisedgastillog@gmail.com>
2018-06-14 23:17:11 -0400

    Toolbar and flexlayoutmodule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+
+/* MATERIAL */
+import { MaterialModule } from './material/material.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+     FlexLayoutModule
   ],
+ // exports: [MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
